8bb57da5fd22be4cdcdcb90fbd3811c564b1999f
SANKETJAIN1111 <sigmasanket2712@gmail.com>
2022-01-12 14:01:17 +0530

    Second Day Commit

---
+++ Sanket_Jain_Day3/src/Day3/Movie.java
@@ -0,0 +1,72 @@
+package Day3;
+
+public class Movie implements MovieInterface{
+
+	
+    private String movieID;
+	
+
+	private String movieName;
+	
+
+	private String producedBy;
+	private String directedBy;
+	private double duration;
+	private int year;
+	private String catagory;
+	static int movieCount = 0;
+	
+	public Movie(String movieName, String producedBy) {
+		
+		this.movieName = movieName;
+		this.producedBy = producedBy;
+	}
+	public Movie(String movieName, String producedBy, String directedBy, double duration, int year, String catagory) {
+	
+		this.movieName = movieName;
+		this.producedBy = producedBy;
+		this.directedBy = directedBy;
+		this.duration = duration;
+		this.year = year;
+		this.catagory = catagory;
+		
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public String getProducedBy() {
+		return producedBy;
+	}
+	public String getDirectedBy() {
+		return directedBy;
+	}
+	public double getDuration() {
+		return duration;
+	}
+	public int getYear() {
+		return year;
+	}
+	public String getCatagory() {
+		return catagory;
+	}
+	public static int getMovieCount() {
+		return movieCount;
+	}
+	final public String getMovieID() {
+		return movieName+"_"+movieCount;
+	}
+	
+	@Override
+	public String toString() {
+		movieCount++;
+		return "Movie [movieName=" + movieName + ", producedBy=" + producedBy + ", directedBy=" + directedBy
+				+ ", duration=" + duration + ", year=" + year + ", catagory=" + catagory + "Total Movie Count : "+movieCount+" MovieId : "+getMovieID()+" ]";
+	}
+	public static void showDetails(Movie m) {
+		System.out.println(m.toString()+"\n");
+		 
+	}
+	public static void showDetails(MovieInterface m) {
+		System.out.println(m.toString()+"\n");
+	}
+}
---
+++ Sanket_Jain_Day3/src/Day3/SpecialMovie.java
@@ -0,0 +1,55 @@
+package Day3;
+
+public class SpecialMovie extends Movie implements MovieInterface, InternationalMovieInterface{
+	
+	private String soundEffect;
+	private String visualEffect;
+	public String getSoundEffect() {
+		return soundEffect;
+	}
+
+	public void setSoundEffect(String soundEffect) {
+		this.soundEffect = soundEffect;
+	}
+
+	public String getVisualEffect() {
+		return visualEffect;
+	}
+
+	public void setVisualEffect(String visualEffect) {
+		this.visualEffect = visualEffect;
+	}
+	public SpecialMovie(String movieName, String producedBy, String soundEffect, String visualEffect) {
+		super(movieName, producedBy);
+		this.soundEffect = soundEffect;
+		this.visualEffect = visualEffect;
+	}
+
+	public SpecialMovie(String movieName, String producedBy, String directedBy, double duration, int year,
+			String catagory,String soundEffect,String visualEffect) {
+		super(movieName, producedBy, directedBy, duration, year, catagory);
+		this.soundEffect = soundEffect;
+		this.visualEffect = visualEffect;
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "SpecialMovie [soundEffect=" + soundEffect + ", visualEffect=" + visualEffect + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	@Override
+	public String getCountryName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLanguage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+
+}
