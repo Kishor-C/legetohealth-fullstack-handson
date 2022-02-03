e4b4f48c3ac6e3cba361f534c4edc5caee8218ee
SANKETJAIN1111 <sigmasanket2712@gmail.com>
2022-02-03 14:14:48 +0530

    Final Commit : Spring core Day 15

--- Angular_Day13/form-data/src/app/mdf-demo/mdf-demo.component.ts
+++ Angular_Day13/form-data/src/app/mdf-demo/mdf-demo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { first } from 'rxjs';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-mdf-demo',
@@ -10,13 +11,14 @@ import { first } from 'rxjs';
 })
 export class MdfDemoComponent  {
 
-  constructor(private _builder:FormBuilder, private _router : Router) { }
+  constructor(private _builder:FormBuilder,private _service: EmployeeService, private _router : Router) { }
     profile=this._builder.group({
     firstname:['', Validators.compose([Validators.required, Validators.minLength(3)])],
     lastname:['', Validators.compose([Validators.required, Validators.minLength(2)])]
   });
   handleSubmit():void{
     let firstname = this.profile.controls['firstname'].value;
+    // if(this._service.fetchEmployee(firstname).subscribe(res=>{},err=>{}))
     if(firstname=='Alex')
     {
     sessionStorage.setItem('user', firstname);
